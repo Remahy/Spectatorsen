08791c1dcce638d50c522818d1113b9dc613e5da
Karar Al-Remahy <alremahykarar@ymail.com>
2026-01-14 03:38:26 +0100

    feat: Twitch bot

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,16 @@
-import { REGIONS } from "./regions.js";
+import {
+  ChatClient,
+  PrivmsgMessage,
+  PrivmsgMessageRateLimiter,
+  SlowModeRateLimiter,
+} from "@mastondzn/dank-twitch-irc";
+import { regionKeys, REGIONS } from "./regions.js";
 
 import game, { Player } from "./spectate.js";
+import { TwitchAuth } from "./token.js";
+
+import pkg from "../pkgObject.cjs";
+const { name: pgkName, version, repository } = pkg;
 
 const spectateInterval = () => {
   clearInterval(spectateInterval);
@@ -14,6 +24,14 @@ const {
   GAME_NAME,
   TAG_LINE,
   SPECTATE_REGION,
+
+  TWITCH_ENABLE,
+  TWITCH_USERNAME,
+
+  TWITCH_CLIENT_ID,
+  TWITCH_CLIENT_SECRET,
+  TWITCH_ACCESS_TOKEN,
+  TWITCH_REFRESH_TOKEN,
 } = process.env;
 
 const spectatePlayer = async (gameName, tagLine, region) => {
@@ -31,10 +49,133 @@ const spectatePlayer = async (gameName, tagLine, region) => {
   return player;
 };
 
+/**
+ * @type {ChatClient}
+ */
+let chat;
+
 (async () => {
-  const region = REGIONS[SPECTATE_REGION.toUpperCase()];
+  if (!TWITCH_ENABLE) {
+    const region = REGIONS[SPECTATE_REGION.toUpperCase()];
 
-  await spectatePlayer(GAME_NAME, TAG_LINE, region);
+    await spectatePlayer(GAME_NAME, TAG_LINE, region);
 
-  spectateInterval();
+    spectateInterval();
+
+    return;
+  }
+
+  const api = new TwitchAuth({
+    clientSecret: TWITCH_CLIENT_SECRET,
+    clientToken: TWITCH_CLIENT_ID,
+    scope: ["chat:edit", "chat:read"],
+    headers: {
+      "user-agent": `${pgkName}${version} (${repository.url})`,
+    },
+  });
+
+  /**
+   * @param {ChatClient} chat
+   * @param {PrivmsgMessage} msg
+   */
+  const spectateCommandParse =
+    (chat, msg) =>
+    /**
+     * @param {string} region
+     * @param {string} value
+     */
+    async (region, value) => {
+      const matchesRegion = REGIONS[region.toUpperCase()];
+
+      if (!matchesRegion) {
+        return chat.reply(
+          msg.channelName,
+          msg.messageID,
+          `🦆 possible region values: ${regionKeys.join(" ")}`
+        );
+      }
+
+      const [gameName, tagLine] = value.split("#");
+
+      const player = await spectatePlayer(gameName, tagLine, matchesRegion);
+
+      spectateInterval();
+
+      return chat.reply(
+        msg.channelName,
+        msg.messageID,
+        `🦆 now spectating "${player.gameName}#${player.tagLine}" in region ${player.region.platform}.`
+      );
+    };
+
+  /**
+   * @param {ChatClient} chat
+   * @param {PrivmsgMessage} msg
+   */
+  const commandParse = (chat, msg) => {
+    const [rawCommand, region, ...player] = msg.messageText.trim().split(" ");
+
+    if (!rawCommand.startsWith("@@")) {
+      return;
+    }
+
+    const command = rawCommand.slice(2);
+
+    if (command === "spectate") {
+      spectateCommandParse(chat, msg)(region, player.join(" "));
+      return;
+    }
+  };
+
+  const resetChat = async () => {
+    if (chat) {
+      chat.removeAllListeners();
+      chat.close();
+    }
+
+    let token = TWITCH_ACCESS_TOKEN;
+    const validToken = await api.validateToken(TWITCH_ACCESS_TOKEN);
+
+    if (validToken === null) {
+      const res = await api.refreshToken(TWITCH_REFRESH_TOKEN);
+      token = res.access_token;
+    }
+
+    chat = new ChatClient({
+      password: `oauth:${token}`,
+      rateLimits: "default",
+      username: TWITCH_USERNAME,
+      connection: {
+        type: "websocket",
+        secure: true,
+      },
+    });
+
+    chat.use(new SlowModeRateLimiter(chat));
+    chat.use(new PrivmsgMessageRateLimiter(chat));
+
+    chat.on("ready", () => console.log("Successfully connected to chat"));
+
+    chat.on("close", (error) => {
+      if (error != null) {
+        console.error("Client closed due to error", error);
+      }
+    });
+
+    chat.on("PRIVMSG", (msg) => {
+      if (msg.isMod) {
+        commandParse(chat, msg);
+      }
+    });
+
+    // See below for more events
+    chat.connect();
+    chat.join(TWITCH_USERNAME);
+  };
+
+  setInterval(() => {
+    resetChat();
+  }, 1_800_000);
+
+  resetChat();
 })();
---
+++ src/token.js
@@ -0,0 +1,120 @@
+// Based off of https://github.com/KararTY/BeFriendlier-Shared/blob/master/src/TwitchAuth.ts
+
+/**
+ * @typedef {Object} TwitchAuthBody
+ * @property {string} access_token
+ * @property {string} refresh_token
+ * @property {number} expires_in
+ * @property {string[]} scope
+ * @property {string} token_type
+ */
+
+/**
+ * @typedef {Object} TwitchValidateBody
+ * @property {string} client_id
+ * @property {number} expires_in
+ * @property {string} login
+ * @property {string[]} scope
+ * @property {string} user_id
+ */
+
+/**
+ * @typedef {Object} Config
+ * @property {string} clientToken
+ * @property {string} clientSecret
+ * @property {string} redirectURI
+ * @property {string[]} scope
+ * @property {Headers} headers
+ */
+
+export class TwitchAuth {
+  /**
+   * @param {Config} config
+   */
+  constructor(config) {
+    this.clientToken = config.clientToken;
+    this.clientSecret = config.clientSecret;
+    this.redirectURI = config.redirectURI;
+    this.scope = config.scope.join("%20");
+    this.headers = config.headers;
+  }
+
+  /**
+   * @returns {Promise<TwitchAuthBody|null>}
+   */
+  async requestAppToken() {
+    const searchParams = new URLSearchParams({
+      client_id: this.clientToken,
+      client_secret: this.clientSecret,
+      grant_type: "client_credentials",
+    });
+
+    try {
+      const res = await fetch(
+        `https://id.twitch.tv/oauth2/token?${searchParams.toString()}&scope=${
+          this.scope
+        }`,
+        {
+          method: "POST",
+          headers: { ...this.headers },
+        }
+      );
+
+      return res.json();
+    } catch (error) {
+      this.logger.error({ err: error }, "Twitch.requestAppToken()");
+      return null;
+    }
+  }
+
+  /**
+   * @param {string} token
+   * @returns {Promise<TwitchAuthBody|null>}
+   */
+  async refreshToken(token) {
+    const searchParams = new URLSearchParams({
+      client_id: this.clientToken,
+      client_secret: this.clientSecret,
+      grant_type: "refresh_token",
+      refresh_token: encodeURI(token),
+    });
+
+    try {
+      const res = await fetch(
+        `https://id.twitch.tv/oauth2/token?${searchParams.toString()}&scope=${
+          this.scope
+        }`,
+        {
+          method: "POST",
+          headers: { ...this.headers },
+        }
+      );
+
+      return res.json();
+    } catch (error) {
+      this.logger.error({ err: error }, "Twitch.refreshToken()");
+      return null;
+    }
+  }
+
+  /**
+   * @param {string} token
+   * @returns {Promise<TwitchValidateBody|null>}
+   */
+  async validateToken(token) {
+    try {
+      const res = await fetch("https://id.twitch.tv/oauth2/validate", {
+        headers: {
+          ...this.headers,
+          "Client-ID": this.clientToken,
+          Authorization: `OAuth ${token}`,
+        },
+      });
+
+      return res.json();
+    } catch (error) {
+      this.logger.error({ err: error }, "Twitch.validateToken()");
+      return null;
+    }
+  }
+}
