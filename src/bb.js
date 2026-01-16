303044397df6a0f73fa7e5faae59a0d5df2786d7
Karar Al-Remahy <alremahykarar@ymail.com>
2026-01-16 15:45:30 +0100

    fix: Delete teams, create new ones & use their ids

--- src/bb.js
+++ src/bb.js
@@ -1,10 +1,78 @@
 import { fetch } from "undici";
 
-const {
-  BLUEBOTTLE_ENABLE,
-  BLUEBOTTLE_ENDPOINT,
-  BLUEBOTTLE_STYLE,
-} = process.env;
+const { BLUEBOTTLE_ENABLE, BLUEBOTTLE_ENDPOINT, BLUEBOTTLE_STYLE } =
+  process.env;
+
+export const resetCurrentGame = async () => {
+  if (!BLUEBOTTLE_ENABLE) {
+    return;
+  }
+
+  await Promise.allSettled([
+    fetch(`${BLUEBOTTLE_ENDPOINT}/api/match/current`, {
+      method: "POST",
+    }),
+  ]);
+};
+
+export const resetTeams = async () => {
+  const defaultTeam1 = {
+    members: [],
+    name: "Blue",
+    tag: "BLUE",
+    isActive: true,
+    primaryColor: "#FFFFFFFF",
+    secondaryColor: "#FFFFFFFF",
+    tertiaryColor: "#FFFFFFFF",
+    backgroundColor: "#FF000000",
+  };
+
+  const defaultTeam2 = {
+    members: [],
+    name: "Red",
+    tag: "RED",
+    isActive: true,
+    primaryColor: "#FFFFFFFF",
+    secondaryColor: "#FFFFFFFF",
+    tertiaryColor: "#FFFFFFFF",
+    backgroundColor: "#FF000000",
+  };
+
+  try {
+    const res = await fetch(`${BLUEBOTTLE_ENDPOINT}/api/team`);
+
+    if (!res.ok) {
+      throw new Error("Couldn't find teams.");
+    }
+
+    const teams = await res.json();
+
+    const teamIds = teams.map(({ teamId }) => teamId);
+
+    await Promise.allSettled(
+      teamIds.map((teamId) =>
+        fetch(`${BLUEBOTTLE_ENDPOINT}/api/team/${teamId}`, {
+          method: "DELETE",
+        })
+      )
+    );
+  } catch {
+    // noop
+  }
+
+  return (
+    await Promise.allSettled([
+      fetch(`${BLUEBOTTLE_ENDPOINT}/api/team`, {
+        method: "POST",
+        body: JSON.stringify(defaultTeam1),
+      }),
+      fetch(`${BLUEBOTTLE_ENDPOINT}/api/team`, {
+        method: "POST",
+        body: JSON.stringify(defaultTeam2),
+      }),
+    ])
+  ).map(({ value }) => value);
+};
 
 export const setBBDefaults = async () => {
   if (!BLUEBOTTLE_ENABLE) {
@@ -12,10 +80,13 @@ export const setBBDefaults = async () => {
   }
 
   const defaultMatch = {
-    name: "",
-    type: 1,
-    teams: [1, 2],
+    teams: [],
     isCurrent: true,
+    seasonId: 1,
+    isActive: true,
+    type: 1,
+    ruleSet: 0,
+    onStage: false,
   };
 
   const defaultShowing = {
@@ -56,10 +127,14 @@ export const setBBDefaults = async () => {
     }),
   ]);
 
+  await resetCurrentGame();
+
+  const teams = await resetTeams();
+
   await Promise.allSettled([
     fetch(`${BLUEBOTTLE_ENDPOINT}/api/match`, {
       method: "PUT",
-      body: JSON.stringify(defaultMatch),
+      body: JSON.stringify({ ...defaultMatch, teams }),
     }),
   ]);
 
